262430c8881cc9391a96fbf993e5490e4d048ee3
Aaron Schuster <aaronjschuster@gmail.com>
2025-02-11 19:46:56 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './styles/App.css';
+
+import discordLogo from "./discord.png"
+import { IoMdDownload as DownloadIcon } from "react-icons/io";
+
+const downloadlink = "https://e.pcloud.link/publink/show?code=XZrtqKZYWdUeMSbrzYdGnuqyliYYVeYDTQy#returl=https%3A//e.pcloud.link/publink/show%3Fcode%3DXZrtqKZYWdUeMSbrzYdGnuqyliYYVeYDTQy&page=login";
+const discordLink = "https://discord.gg/Dx872GJW";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Reign of Victory</h1>
+      </header>
+      <p>
+        Very early Alpha version of the game "Reign of Victory" released for a premature early access.
+        Please provide feedback on the Discord!
+      </p>
+      <div className="links">
+        <a className="downloadlink" href={downloadlink}>
+          <div className="downloadlinkdiv">
+            <p>Download</p>
+            <DownloadIcon className="downloadicon"/>
+          </div>
+        </a>
+        <a className="discordlink" href={discordLink}>
+          <div className="discordlinkdiv">
+            <p>Join the Discord!</p>
+            <img className="discordlogo" src={discordLogo} alt="Discord logo"/>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
