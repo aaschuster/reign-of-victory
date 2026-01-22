67cc34249d2c82f356b1b25f504694badba60111
Aaron Schuster <aaronjschuster@gmail.com>
2026-01-22 15:05:17 -0500

    added all new links

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import discordLogo from "./discord.png"
 import youtubeLogo from "./youtube.png"
 import twitchLogo from "./twitch.png"
 import steamLogo from "./steam.png"
+import fbLogo from "./facebook.png"
+import xLogo from "./x.png"
+import blueskyLogo from "./bluesky.png"
+import redditLogo from "./reddit.png"
+import tiktokLogo from "./tiktok.png"
 
 import wordmark from "./wordmark.png"
 import banner from "./banner.png"
 import king from "./king.png"
 
 const discordLink = "https://discord.com/invite/rEWtzeQzNu";
-const youtubeLink = "https://youtu.be/fB9uTvCWwlw";
+const youtubeLink = "https://www.youtube.com/@reignofvictory";
 const twitchLink = "https://m.twitch.tv/reignofvictorygaming/home?tt_content=channel&tt_medium=mobile_web_share";
 const steamLink = "https://store.steampowered.com/app/3890650/Reign_of_Victory/?beta=0";
+const fbLink = "https://www.facebook.com/people/Reign-of-Victory/61581087128167/";
+const xLink = "https://x.com/reign_ofvictory";
+const blueskyLink = "https://bsky.app/profile/reignofvictory.bsky.social";
+const tiktokLink = "https://www.tiktok.com/@reign_of_victory";
+const redditLink ="https://www.reddit.com/r/ReignofVictory/";
+const infoText = "Build and trade, fight and conquer or road and steal in this multiplayer open-world RPG where you can choose your own adventure by taking part as a villager, knight or outlaw to become the victor!"
 
 function App() {
 
@@ -27,9 +38,12 @@ function App() {
   return (
       <div className="app" style={{backgroundImage: `url(${banner})`}}>
         <header>
-          <img className="king" src={king} alt="Regal King with a golden crown sitting on a large ornate throne"/>
           <img className="wordmark" src={wordmark} alt="Reign of Victory wordmark"/>
         </header>
+        <div className="info">
+          <img className="king" src={king} alt="Regal King with a golden crown sitting on a large ornate throne"/>
+          <p>{infoText}</p>
+        </div>
         <div className="links">
           <a className="steamlink" href={steamLink}>
             <div className="linkdiv steamlinkdiv">
@@ -37,16 +51,45 @@ function App() {
               <p>Wishlist Now!</p>
             </div>
           </a>
+          <a className="fblink" href={fbLink}>
+            <div className="linkdiv fblinkdiv">
+              <img className="fblogo" src={fbLogo} alt="Facebook logo"/>
+              <p>Facebook</p>
+            </div>
+          </a>
+          <a className="xLink" href={xLink}>
+            <div className="linkdiv xlinkdiv">
+              <img className="xlogo" src={xLogo} alt="X logo"/>
+            </div>
+          </a>
+          <a className="blueskylink" href={blueskyLink}>
+            <div className="linkdiv blueskylinkdiv">
+              <img className="blueskylogo" src={blueskyLogo} alt="BlueSky logo"/>
+              <p>BlueSky</p>
+            </div>
+          </a>
+          <a className="redditlink" href={redditLink}>
+            <div className="linkdiv redditlinkdiv">
+              <img className="redditlogo" src={redditLogo} alt="Reddit logo"/>
+              <p>Reddit</p>
+            </div>
+          </a>
+          <a className="tiktoklink" href={tiktokLink}>
+            <div className="linkdiv tiktoklinkdiv">
+              <img className="tiktoklogo" src={tiktokLogo} alt="TikTok logo"/>
+              <p>Tiktok</p>
+            </div>
+          </a>
           <a className="discordlink" href={discordLink}>
             <div className="linkdiv discordlinkdiv">
               <img className="discordlogo" src={discordLogo} alt="Discord logo"/>
-              <p>Join the Discord!</p>
+              <p>Discord</p>
             </div>
           </a>
           <a className="youtubelink" href={youtubeLink}>
             <div className="linkdiv youtubelinkdiv">
               <img className="youtubelogo" src={youtubeLogo} alt="YouTube logo"/>
-              <p>Trailer</p>
+              <p>YouTube</p>
             </div>
           </a>
           <a className="twitchlink" href={twitchLink}>
